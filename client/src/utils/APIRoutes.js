c8f99248fd79ef2a6dc549506f49a5c956362fa8
sanskar1306 <sanskarkabra1306@gmail.com>
2023-08-25 14:14:57 +0530

    Api route change

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://snappy-chat-backend-123.onrender.com";
-export const host = "http://localhost:5000";
+export const host = process.env.HOST;
+// export const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
